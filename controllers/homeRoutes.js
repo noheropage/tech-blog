ce602d709ddd2596a4d4039402e83bd9704c7798
noheropage <mursepage@gmail.com>
2021-05-04 14:49:41 -0700

    Styling for nav bar and posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,7 +6,13 @@ const withAuth = require('../utils/auth');
 router.get('/', async (req, res) => {
   try {
     //   create call for all blog posts
-    const postData = await Post.findAll({});
+    const postData = await Post.findAll({
+      include: [
+        { 
+          model: User, attributes: ["username"] 
+        }
+      ]
+    });
 
     const posts = postData.map((post) => post.get({ plain: true }));
 
